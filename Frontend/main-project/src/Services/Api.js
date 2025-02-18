b344f5e34d758763f584765aea2b9e73399c9603
rafael-men <rafaelgoncalves1822@gmail.com>
2025-02-18 18:13:57 -0300

    frontend adjustment

--- Frontend/main-project/src/Services/Api.js
+++ Frontend/main-project/src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/veiculo',
+  baseURL: 'https://fullstack-vehicle-management-backend.onrender.com/veiculo',
 });
 
 export const getAllVehicles = () => api.get();
